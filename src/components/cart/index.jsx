3fb7e341efcd3565ec2584b79abcb3404f2933f7
pjmantoss <joelptoss@gmail.com>
2020-04-08 16:20:02 +0100

    add images and update index.jsx

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -4,19 +4,19 @@ const items = [
     {
         sku: "sku_H3m1A1FYmkFTn6", 
         quantity: 1, 
-        price: 1798, 
+        price: 179800, 
         name: "Sony Alpha A6600 Mirrorless Camera with 18-135mm Zoom Lens"
     },
     {
         sku: "sku_H3lsYoG0g7UpYi", 
         quantity: 1, 
-        price: 2499, 
+        price: 249900, 
         name: "Canon EOS 5D Mark IV Full Frame Digital SLR Camera Body by Canon"
     },
     {
         sku: "sku_H3ln5Gk53xzAXH", 
         quantity: 1, 
-        price: 1199, 
+        price: 119900, 
         name: "Blackmagic Design Production Camera 4K with EF Mount"
     }
 ];
@@ -35,7 +35,11 @@ export default function Cart({stripeToken}) {
                 </thead>
 
                 <tbody>
-                    {}
+                    {
+                        items.map(item => (
+                            <tr></tr>
+                        ))
+                    }
                 </tbody>
             </table>
         </div>
