a1ff1ae084d9b8b6ef58cfaca42df72d3aa726e5
pjmantoss <joelptoss@gmail.com>
2020-04-08 16:50:59 +0100

    add stripeToken

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import Cart from './components/cart';
 
 ReactDOM.render(
-  <Cart />,
+  <Cart stripeToken="pk_test_YO4hCduILG2Vkvaecq4Th13V007ILyXZUw" />,
   document.getElementById('root')
 );
