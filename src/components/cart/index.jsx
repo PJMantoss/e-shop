9134128cf0a2952d3bd60cf674742d07241fb1aa
pjmantoss <joelptoss@gmail.com>
2020-04-08 15:53:59 +0100

    add yarn and update index.js

---
+++ src/components/cart/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Cart({stripeToken}) {
+    return (
+        <div>
+            Cart
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Cart from './components/cart';
 
 ReactDOM.render(
-  <App />,
+  <Cart />,
   document.getElementById('root')
 );
