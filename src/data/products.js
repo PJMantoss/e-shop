8141b26f6ed0dd38a14624c4e7101c969ec47a43
pjmantoss <joelptoss@gmail.com>
2020-04-10 19:07:22 +0100

    add more products in products.js

--- src/data/products.js
+++ src/data/products.js
@@ -16,6 +16,24 @@ const products = [
         quantity: 1, 
         price: 119900, 
         name: "Blackmagic Design Production Camera 4K with EF Mount"
+    },
+    {
+        sku: "sku_H4a16UOxOevXuA", 
+        quantity: 1, 
+        price: 104999, 
+        name: "Autel Robotics - EVO 4K Drone with Controller - Orange"
+    },
+    {
+        sku: "sku_H4aDRZPra4mEaE", 
+        quantity: 1, 
+        price: 159900, 
+        name: "Phantom 4 Pro v2.0"
+    },
+    {
+        sku: "sku_H4aLVFI0muVevG", 
+        quantity: 1, 
+        price: 15617, 
+        name: "Holy Stone GPS FPV RC Drone HS100 with 1080P Camera Live Video and GPS"
     }
 ];
 
